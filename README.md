6d0e2c9093a8295709831f7298ddf00fc92177da
Shawn Ma <qsma@thoughtworks.com>
2015-11-06 10:04:37 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+## ThoughtWorks Docker Image: mesos
+
+[![](http://dockeri.co/image/baselibrary/mesos)](https://registry.hub.docker.com/u/baselibrary/mesos/)
+
+### Base Docker Image
+
+* `latest`: mesos 0.25.0
+* `0.25.0`: mesos 0.25.0
+* `0.23.0`: mesos 0.23.0
+
+### Installation
+
+    docker pull baselibrary/mesos
+
+### Usage
+
+    docker run -it --rm baselibrary/mesos ...
---
+++ update.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+versions=( "$@" )
+if [ ${#versions[@]} -eq 0 ]; then
+	versions=( */ )
+fi
+versions=( "${versions[@]%/}" )
+
+
+for version in "${versions[@]}"; do	
+  fullVersion="$(curl -fsSL "http://repos.mesosphere.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -v pkgname="mesos" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
+	(
+		set -x
+		cp docker-entrypoint.sh "$version/"
+		sed '
+			s/%%MESOS_MAJOR%%/'"$version"'/g;
+			s/%%MESOS_VERSION%%/'"$fullVersion"'/g;
+		' Dockerfile.template > "$version/Dockerfile"
+	)
+done
