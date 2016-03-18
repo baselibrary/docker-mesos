9cd353b4219f2d1ce8d8541e233b1e30fb231fad
Shawn Ma <qsma@thoughtworks.com>
2016-03-18 23:51:46 +0800

    更新编译脚本

--- update.sh
+++ update.sh
@@ -10,8 +10,9 @@ fi
 versions=( "${versions[@]%/}" )
 
 
-for version in "${versions[@]}"; do	
-  fullVersion="$(curl -fsSL "http://repos.mesosphere.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz" | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg ~ /^mesos$/ && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
+for version in "${versions[@]}"; do
+	repoPackage="http://repos.mesosphere.com/ubuntu/dists/trusty/main/binary-amd64/Packages.gz"
+  fullVersion="$(curl -fsSL "${repoPackage}" | gunzip | awk -v pkgname="mesos" -F ': ' '$1 == "Package" { pkg = $2 } pkg == pkgname && $1 == "Version" { print $2 }' | grep "^$version" | sort -rV | head -n1 )"
 	(
 		set -x
 		cp docker-entrypoint.sh "$version/"
